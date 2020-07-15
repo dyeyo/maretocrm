cd9093a5e933d4db87388b0578d282291eb7d18b
dyego vallejo <dyegovallejob@gmail.com>
2020-07-15 09:02:17 -0500

    librerias

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,6 @@
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
   <script src="{{ asset('js/perfect-scrollbar.jquery.min.js') }}"></script>
-  <script src="{{ asset('js/perfect-scrollbar.jquery.min.js') }}"></script>
   <script src="{{ asset('js/waves.js') }}"></script>
   <script src="{{ asset('js/sidebarmenu.js') }}"></script>
   <script src="{{ asset('js/custom.min.js') }}"></script>
@@ -62,8 +61,6 @@
   <script src="{{ asset('extras/flot/jquery.flot.js') }}"></script>
   <script src="{{ asset('extras/flot.tooltip/js/jquery.flot.tooltip.min.js') }}"></script>
   <script src="{{ asset('extras/sparkline/jquery.sparkline.min.js') }}"></script>
-  <script src="{{ asset('extras/jquery.easy-pie-chart/dist/jquery.easypiechart.min.js') }}"></script>
-  <script src="{{ asset('extras/jquery.easy-pie-chart/easy-pie-chart.init.js') }}"></script>
   <script src="{{ asset('extras/datatables/jquery.dataTables.min.js') }}"></script>
   <script src="{{ asset('js/dashboard2.js') }}"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/js/select2.min.js" defer></script>
